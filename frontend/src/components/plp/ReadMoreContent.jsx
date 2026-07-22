ee9b53aa09dc64d0a33a40696bd67d8e486aa01e
Jai <jai.sarki@navgrahaa.com>
2026-07-22 19:42:54 +0530

    made responsive read more section and integrated api data

--- frontend/src/components/plp/ReadMoreContent.jsx
+++ frontend/src/components/plp/ReadMoreContent.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-const ReadMoreContent = () => {
+const ReadMoreContent = ({ data }) => {
+  console.log("ReadMoreContent data:", data);
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div>ReadMoreContent</div>
-  )
-}
+    <section className={` transition-all duration-500 `}>
+      <div
+        className={`mx-auto flex max-w-7xl flex-col justify-center px-5 py-4 lg:py-12 md:px-8 lg:px-12 transition-all duration-500 ${
+          expanded ? "lg:min-h-[338px]" : "lg:min-h-[314px]"
+        }`}
+      >
+        {/* Heading */}
+        <h2 className="text-center font-serif text-2xl leading-tight text-[#1D1D1D] lg:text-4xl lg:leading-15">
+          {data?.title}
+        </h2>
 
-export default ReadMoreContent
+        {/* Description */}
+        <div className="mx-auto mt-8 max-w-5xl">
+          <p className="text-center line-clamp-3  text-[16px] lg:leading-6 text-[#262626] ">
+            {data?.previewContent}
+          </p>
+
+          {expanded && (
+            <div>
+              <h2 className="text-center font-serif text-[38px] leading-tight text-[#1D1D1D] lg:text-[48px] lg:leading-15">
+                {data?.expandedTitle}
+              </h2>
+
+              <p className="text-center text-[16px] lg:leading-8 text-[#262626] ">
+                {data?.expandedContent}
+              </p>
+            </div>
+          )}
+
+          <div className="mt-4 flex justify-center">
+            <button
+              onClick={() => setExpanded(!expanded)}
+              className="text-[18px] hover:cursor-pointer font-medium text-[#A0704F] transition "
+            >
+              {expanded ? data?.readLessLabel : data?.readMoreLabel}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ReadMoreContent;
