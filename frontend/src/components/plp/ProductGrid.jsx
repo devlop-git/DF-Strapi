49bb4e43dd7f282706eaf2a0d0f836a6b8dba7a8
Devanshi <devanshieupabheda@gmail.com>
2026-07-22 18:05:02 +0530

    added filter mapping wih badges

--- frontend/src/components/plp/ProductGrid.jsx
+++ frontend/src/components/plp/ProductGrid.jsx
@@ -1,10 +1,10 @@
 import ProductCard from "./ProductCard";
 
-export default function ProductGrid({ products }) {
+export default function ProductGrid({ products, filters }) {
   return (
     <div className="grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4">
       {products.map((product) => (
-        <ProductCard key={product.ornamentId} product={product} />
+        <ProductCard key={product.ornamentId} product={product} filters={filters} />
       ))}
     </div>
   );
