1910fa66b8c7cb9670d78aa97334c89b364accce
Jai <jai.sarki@navgrahaa.com>
2026-07-23 23:36:20 +0530

    added hide show in product grid

--- frontend/src/components/plp/ProductGrid.jsx
+++ frontend/src/components/plp/ProductGrid.jsx
@@ -11,26 +11,28 @@ export default function ProductGrid({
 }) {
   return (
     <div className="mx-5 lg:mx-0">
-      <div className="hidden lg:flex justify-between">
-        <h1>
+      <div className="flex justify-between">
+        <h1 className="text-sm my-2 lg:my-0">
           {config.showProductCount && <span>{totalProducts} Products</span>}
         </h1>
 
-        {config.showSort && (
-          <div className="flex items-center gap-3">
-            <select
-              value={selectedSort}
-              onChange={(e) => onSortChange(e.target.value)}
-              className=" px-3 py-2 text-sm"
-            >
-              {sortOptions.map((option) => (
-                <option key={option.id} value={option.id}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
+        <div className="hidden lg:block">
+          {config.showSort && (
+            <div className="flex items-center gap-3">
+              <select
+                value={selectedSort}
+                onChange={(e) => onSortChange(e.target.value)}
+                className=" px-3 py-2 text-sm"
+              >
+                {sortOptions.map((option) => (
+                  <option key={option.id} value={option.id}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
+        </div>
       </div>
       <div className="grid grid-cols-2 gap-x-3 lg:gap-x-4 gap-y-10 lg:grid-cols-3">
         {products.map((product) => (
